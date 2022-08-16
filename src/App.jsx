5596a3f7b2fe6096a760db1587f52b3e5ed24e2c
CarmenXP <xocuaposadas.carmen@gmail.com>
2022-08-16 00:45:18 -0500

    input autocomplete

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,14 @@ import LocationInfo from './components/LocationInfo'
 
 
 function App() {
+  //states
   const [inputS, setInputS] = useState('')
   const [location, setlocation] = useState()
+  const [data, setData] = useState()
+  const [wordEntered, setWordEntered]= useState("")
+  const [filterData, setFilterData] = useState([])
 
-
+  // life cicles
  useEffect(() =>{
   let numberLocation
   if(inputS=== ''){
@@ -26,13 +30,46 @@ function App() {
 
  
  }, [inputS])
+ useEffect(() => {
+  const URL = "https://rickandmortyapi.com/api/location"
+  axios.get(URL)
+  .then(res => setData(res.data.results))
+  .catch(err => console.log(err))
 
+ }, [])
+ console.log("data", data)
+ 
+/** 
+ const cleanInput =()=>{
+  setInputS("")
+ }
+ **/
+// Handles
  const handleSumit = e =>{
   e.preventDefault()
   setInputS(e.target.search.value)
  }
-console.log("input", typeof inputS)
+ const handleFilter = e =>{
+  const searchWord=e.target.value
+  setWordEntered(searchWord)
+  const filterWord = data.filter(x =>{
+    return x.name.toLowerCase().includes(searchWord.toLowerCase())
+  })
 
+  if(searchWord === ""){
+    setFilterData([])
+  }else{
+    setFilterData(filterWord)
+  }
+ }
+console.log("input", typeof inputS)
+console.log("desde el filtrado",filterData)
+
+/** 
+ const cleanInput =()=>{
+  setInputS("")
+ }
+ **/
   
   return (
     <div className="App">
@@ -40,9 +77,17 @@ console.log("input", typeof inputS)
         <img src='https://br.web.img3.acsta.net/pictures/17/11/06/14/13/5164749.jpg?coixp=50&coiyp=47'/>
       </div>
         <form onSubmit={handleSumit}>
-          <input id='search' type="text" />
+          <input id='search' type="text" onChange={handleFilter}/>
           <button>Search</button>
         </form>
+        <div className="results">
+         {
+          filterData.length != 0
+            ? filterData.map(data =>{ return(<p>{data.name}</p>)})
+            : ""
+         } 
+        </div>
+       
         <LocationInfo location ={location}/>
         <div className='contentCard'>
           {
